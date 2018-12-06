5b1691ae9616d123e4dfaee3f3908e9e393476c9
douyuehan <Kenny9825@sina.com>
2018-12-06 16:21:38 +0800

    引入公共头部

---
+++ src/main/java/com/neusoft/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.neusoft.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/12/6.
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("reg")
+    public String reg()
+    {
+        return "user/reg";
+    }
+
+    @RequestMapping("login")
+    public String login()
+    {
+        return "user/login";
+    }
+}
